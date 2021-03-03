f9d87c2e01659ce9dce664608604469c208550d8
Nathan Doherty <dohertynathan13@gmail.com>
2021-03-03 10:16:48 -0800

    variables

---
+++ Variables/age.rb
@@ -0,0 +1,7 @@
+puts "How old are you?"
+	name = gets.chomp
+puts 
+"In ten years you will be " + 30 + "years old."
+"In 20 years you will be " + 40 + "years old."
+"In 30 years you will be " + 50 + "years old."
+"In 40 years you will be " + 60 + "years old."
---
+++ Variables/name.rb
@@ -0,0 +1,12 @@
+puts "Hello, what is your name?"
+	name = gets.chomp
+
+print " It is nice to meet you #{name}!"
+
+#OR 
+
+puts "What is your name?"
+	name = gets.chomp
+puts "It is nice to meet you " + name 
+
+
