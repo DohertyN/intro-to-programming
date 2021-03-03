e41c8adae46052496d81113f377aa63558f153ed
Nathan Doherty <dohertynathan13@gmail.com>
2021-03-03 00:00:53 -0800

    basics

---
+++ The_Basics/movie.rb
@@ -0,0 +1,6 @@
+movies = { :amadeus => 1984, :tombstone => 1993, :braveheart => 1995, :firefly => 2005 }
+
+puts movies [:amadeus]
+puts movies [:tombstone]
+puts movies [:braveheart]
+puts movies [:firefly]
---
+++ The_Basics/movie_array.rb
@@ -0,0 +1,8 @@
+years = [1984, 1993, 1995, 2005]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+
+
