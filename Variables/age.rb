fc7fadcde27f9bf55db1a8d465526a7b309a21bb
Nathan Doherty <npdoherty13@gmail.com>
2021-03-05 12:43:44 -0800

    Delete age.rb

--- Variables/age.rb
+++
@@ -1,7 +0,0 @@
-puts "How old are you?"
-	name = gets.chomp
-puts 
-"In ten years you will be " + 30 + "years old."
-"In 20 years you will be " + 40 + "years old."
-"In 30 years you will be " + 50 + "years old."
-"In 40 years you will be " + 60 + "years old."
