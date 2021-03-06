3f6b174ab9bbe3919acc07b3513e9616327fad7b
Nathan Doherty <dohertynathan13@gmail.com>
2021-03-05 17:26:11 -0800

    methods

---
+++ methods/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+    " It sure is swell to meet you " + name + "!"
+end 
+
+puts greeting("Philip II of Macedon")
---
+++ methods/say.rb
@@ -0,0 +1,9 @@
+def say(words= 'Hola')
+  puts words + '.'
+end 
+
+say()
+say("hi")
+say("how are you")
+say("I'm fine")
+
